d018a01e99126c03fb3c3e5d9ce6034596d117eb
Mateus <omateusmento@gmail.com>
2024-11-13 09:41:26 -0300

    Add init.sh script

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo chmod +x ./zeek/install-zeek.sh
+sudo chmod +x ./zeek/include-hash-sha256.sh
+sudo chmod +x ./splunk/install-splunk.sh
+sudo chmod +x ./splunk/install-splunk-forwarder.sh
