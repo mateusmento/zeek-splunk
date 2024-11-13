3a98782ccbe231260be2a5ba2849e9866fa00328
Mateus <omateusmento@gmail.com>
2024-11-13 10:31:10 -0300

    Add install deps

--- init.sh
+++ init.sh
@@ -7,7 +7,8 @@ sudo sed -i 's/\r$//' ./zeek/config-zeek-node
 sudo sed -i 's/\r$//' ./zeek/deploy-zeek
 sudo sed -i 's/\r$//' ./zeek/stop-zeek
 sudo sed -i 's/\r$//' ./zeek/install-zkg
-sudo sed -i 's/\r$//' ./zeek/include-hash-sha256.sh
+sudo sed -i 's/\r$//' ./zeek/install-deps
+sudo sed -i 's/\r$//' ./zeek/check-policy
 sudo sed -i 's/\r$//' ./splunk/install-splunk.sh
 sudo sed -i 's/\r$//' ./splunk/install-splunk-forwarder.sh
 
@@ -18,5 +19,7 @@ sudo chmod +x ./zeek/config-zeek-node
 sudo chmod +x ./zeek/deploy-zeek
 sudo chmod +x ./zeek/stop-zeek
 sudo chmod +x ./zeek/install-zkg
+sudo chmod +x ./zeek/install-deps
+sudo chmod +x ./zeek/check-policy
 sudo chmod +x ./splunk/install-splunk.sh
 sudo chmod +x ./splunk/install-splunk-forwarder.sh
