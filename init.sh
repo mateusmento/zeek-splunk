7783c5b1799153336e36eba98cbe566171ea49c5
Mateus <omateusmento@gmail.com>
2024-11-13 09:48:12 -0300

    Replace end of file chars

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+sudo sed -i 's/\r$//' ./zeek/install-zeek.sh
+sudo sed -i 's/\r$//' ./zeek/include-hash-sha256.sh
+sudo sed -i 's/\r$//' ./splunk/install-splunk.sh
+sudo sed -i 's/\r$//' ./splunk/install-splunk-forwarder.sh
+
 sudo chmod +x ./zeek/install-zeek.sh
 sudo chmod +x ./zeek/include-hash-sha256.sh
 sudo chmod +x ./splunk/install-splunk.sh
