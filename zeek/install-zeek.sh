9468796b086fd213fabb875a62b4d33fd930ae69
Mateus <omateusmento@gmail.com>
2024-11-13 10:35:19 -0300

    Force install deps

--- zeek/install-zeek.sh
+++ zeek/install-zeek.sh
@@ -6,7 +6,7 @@ sudo cat /etc/debian_version
 sudo echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
 sudo curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
 sudo apt -y update
-sudo apt -y install zeek-lts
+sudo apt -y install zeek-lts -f
 sudo setcap cap_net_raw=eip /opt/zeek/bin/zeek
 sudo setcap cap_net_raw=eip /opt/zeek/bin/capstats
 sudo bash -c 'echo -e "ZeekPort = 27760" >> /opt/zeek/etc/zeekctl.cfg'
